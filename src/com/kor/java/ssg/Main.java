71a99a4578c72c6b2b7e395928db9ada6c5a8a90
Sokrisan <dioxer7894@gmail.com>
2022-05-17 19:37:28 +0900

    스캐너 입력받기

---
+++ src/com/kor/java/ssg/Main.java
@@ -0,0 +1,13 @@
+package com.kor.java.ssg;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in);
+		String commend = sc.nextLine();
+		System.out.println("입력된 명령어 : " + commend);
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
